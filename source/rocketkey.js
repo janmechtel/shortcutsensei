a78e2a7034240056a5dd25e522f4065a854c15e8
Jan Mechtel <jme@templafy.com>
2021-10-24 00:12:11 +0200

    added first version of contentscript

---
+++ source/rocketkey.js
@@ -0,0 +1,13 @@
+document.body.style.border = "5px solid red";
+
+document.onclick = function (event) {
+    const innerText = event.explicitOriginalTarget.innerText;
+    const outerHTML = event.explicitOriginalTarget.outerHTML;
+    console.log(`You clicked on: '${innerText}' (innerText)`);
+    console.log(`You clicked on: '${outerHTML}' (outerHTML)`);
+
+    if (innerText == "Compose" || outerHTML.includes("Compose")) {
+        console.log("Try pressing 'C' instead ;-)")
+    }
+    console.debug("Click event: ", event);
+}
