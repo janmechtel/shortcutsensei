86d99bc8edf0ec4b35d224592c2e4fa78b30d982
Jan Mechtel <jme@templafy.com>
2021-10-24 04:02:48 +0200

    switched to typescript linter

--- source/rocketkey.ts
+++ source/rocketkey.ts
@@ -1,32 +1,26 @@
-document.body.style.border = "5px solid yellow";
-// Create a success notification instance
 require('styled-notifications');
 
+document.body.style.border = '5px solid yellow';
+
 const successNotification = window.createNotification({
-	// closeOnClick: true,
-	// displayCloseButton: false,
-	// positionClass: 'nfc-top-right',
-	// onclick: false,
-	// showDuration: 3500,
-	// theme: 'success'
 	positionClass: 'nfc-bottom-right',
-	theme: 'warning'
+	theme: 'warning',
 });
 
-document.onclick = function (event) {
-    const innerText = event.explicitOriginalTarget.innerText;
-    const outerHTML = event.explicitOriginalTarget.outerHTML;
-    console.log(`You clicked on: '${innerText}' (innerText)`);
-    console.log(`You clicked on: '${outerHTML}' (outerHTML)`);
-	console.debug("Click event: ", event);
+const clickHandler = function (event: MouseEvent) {
+	const innerText: string = event.explicitOriginalTarget.textContent;
+	const outerHTML: string = event.explicitOriginalTarget.outerHTML;
+	console.log(`You clicked on: '${innerText}' (innerText)`);
+	console.log(`You clicked on: '${outerHTML}' (outerHTML)`);
+	console.debug('Click event:', event);
 
-    if (innerText == "Compose" || outerHTML.includes("Compose")) {
-        console.log("")
+	if (innerText === 'Innovation' || outerHTML.includes('Compose')) {
 		// Use the same instance but pass a title
 		successNotification({
 			title: 'Press C',
-			message: 'For "Compose" try pressing "C" instead ;-)'
+			message: 'For "Compose" try pressing "C" instead ;-)',
 		});
+	}
+};
 
-    }
-}
+document.addEventListener('click', clickHandler);
