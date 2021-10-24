57b974ef840d6409ef313ff14a797b3b87e9d9b5
Jan Mechtel <jme@templafy.com>
2021-10-24 08:10:02 +0200

    changed remaining .js to .ts

--- source/options.ts
+++ source/options.ts
@@ -1,7 +1,6 @@
-// Don't forget to import this wherever you use it
-import browser from 'webextension-polyfill';
+// Don't forget to import this wherever you use it // import browser from 'webextension-polyfill';
 
-import optionsStorage from './options-storage.js';
+import optionsStorage from './options-storage';
 
 optionsStorage.syncForm('#options-form');
 
