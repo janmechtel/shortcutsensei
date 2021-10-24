ece5b3900d3b6efb0557ed7331a5d04de3f22797
Jan Mechtel <jme@templafy.com>
2021-10-24 02:11:37 +0200

    showing a popup notification

--- source/rocketkey.ts
+++ source/rocketkey.ts
@@ -1,13 +1,32 @@
 document.body.style.border = "5px solid yellow";
+// Create a success notification instance
+require('styled-notifications');
+
+const successNotification = window.createNotification({
+	// closeOnClick: true,
+	// displayCloseButton: false,
+	// positionClass: 'nfc-top-right',
+	// onclick: false,
+	// showDuration: 3500,
+	// theme: 'success'
+	positionClass: 'nfc-bottom-right',
+	theme: 'information'
+});
 
 document.onclick = function (event) {
     const innerText = event.explicitOriginalTarget.innerText;
     const outerHTML = event.explicitOriginalTarget.outerHTML;
     console.log(`You clicked on: '${innerText}' (innerText)`);
     console.log(`You clicked on: '${outerHTML}' (outerHTML)`);
+	console.debug("Click event: ", event);
 
     if (innerText == "Compose" || outerHTML.includes("Compose")) {
-        console.log("Try pressing 'C' instead ;-)")
+        console.log("")
+		// Use the same instance but pass a title
+		successNotification({
+			title: 'Press C',
+			message: 'For "Compose" try pressing "C" instead ;-)'
+		});
+
     }
-    console.debug("Click event: ", event);
 }
