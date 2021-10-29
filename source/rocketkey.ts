67c8342b12be983021820bc2ed1d338a57261335
LennardVaarten <l.a.vaarten@tilburguniversity.edu>
2021-10-29 21:50:52 +0200

    git commit -m "added shortcuts"

--- source/rocketkey.ts
+++ source/rocketkey.ts
@@ -16,15 +16,21 @@ const gmailShortcuts: Shortcut[] = [
 	// button: string; // the content ont the Text or OuterHTML, eg. 'Compose'
 	// )
 	new Shortcut("X", "Innovate a lot! (Test for mozilla)", "Innovation"),
+
 	new Shortcut("C", "Compose", "<div class=\"T-I T-I-KE L3\" style=\"user-select: none\" role=\"button\" tabindex=\"0\" jscontroller=\"eIu7Db\" jsaction=\"click:dlrqf; clickmod:dlrqf\" jslog=\"20510; u014N:cOuCgd,Kr2w4b\" gh=\"cm\">Compose</div>"),
 	new Shortcut("E", "Archive", "<div class=\"asa\"><div class=\"ar8 T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("U", "Go back to inbox", "<div class=\"asa\"><div class=\"ar6 T-I-J3 J-J5-Ji\"></div></div>"), // doesn't work because no descriptive innerText / outerHTML
-	new Shortcut("CTRL+Shift+C", "Add Cc recipients", "Add Cc recipients"), // doesn't work because not registered as click. Is suggested when pressing "Link to"
-	new Shortcut("CTRL+Shift+B", "Add Bcc recipients", "Add Bcc recipients"), // doesn't work because not registered as click. Is suggested when pressing "Link to"
-	new Shortcut("CTRL+Enter", "Send email", "<div id=\":14h\" class=\"T-I J-J5-Ji aoO v7 T-I-atl L3\" role=\"button\" tabindex=\"1\" style=\"user-select: none;\" data-tooltip=\"Send ‪(Ctrl-Enter)‬\" aria-label=\"Send ‪(Ctrl-Enter)‬\" data-tooltip-delay=\"800\" jslog=\"32601; u014N:cOuCgd,Kr2w4b; dYFj7e:true; 11:WyIjbXNnLWE6cjEwNTcyMzg2Mzg1Nzc3NjgxMjQiLG51bGwsbnVsbCxudWxsLDEsbnVsbCxbIiN0aHJlYWQtYTpyLTYxMTY2NjY1NzMzMDY1MDU4NTAiLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLFtdXSxmYWxzZSxudWxsLGZhbHNlLGZhbHNlXQ..\">Send</div>"),
-	new Shortcut("K", "Jump to newer email", "Newer"), // doesn't work; not sure why. 'Newer' is in outerHTML
-	new Shortcut("J", "Jump to older email", "Older"), // doesn't work; not sure why. 'Older' is in outerHTML
-	new Shortcut("CTRL+K", "Insert a link", "Link to") // doesn't work; not sure why. 'Link to' is in outerHTML
+	new Shortcut("U", "Go back to inbox", "<div class=\"asa\"><div class=\"ar6 T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("Ctrl+Enter", "Send", "<div id=\":14h\" class=\"T-I J-J5-Ji aoO v7 T-I-atl L3\" role=\"button\" tabindex=\"1\" style=\"user-select: none;\" data-tooltip=\"Send ‪(Ctrl-Enter)‬\" aria-label=\"Send ‪(Ctrl-Enter)‬\" data-tooltip-delay=\"800\" jslog=\"32601; u014N:cOuCgd,Kr2w4b; dYFj7e:true; 11:WyIjbXNnLWE6cjEwNTcyMzg2Mzg1Nzc3NjgxMjQiLG51bGwsbnVsbCxudWxsLDEsbnVsbCxbIiN0aHJlYWQtYTpyLTYxMTY2NjY1NzMzMDY1MDU4NTAiLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLFtdXSxmYWxzZSxudWxsLGZhbHNlLGZhbHNlXQ..\">Send</div>"),
+	new Shortcut("K", "Jump to newer email", "data-tooltip=\"Newer\">"),
+	new Shortcut("J", "Jump to older email", "data-tooltip=\"Older\">"),
+	new Shortcut("R", "Reply", "<span id =\":177\" role=\"link\" tabindex=\"0\" class=\"ams bkH\" jslog=\"21576; u014N:cOuCgd,Kr2w4b;\">Reply</span>"),
+	new Shortcut("Ctrl+B", "Bold", "<div class=\"eN  aaA aaB\"> </div>"),
+	new Shortcut("Ctrl+I", "Italics", "<div class=\"e3  aaA aaB\"> </div>"),
+	new Shortcut("Ctrl+U", "Underline", "<div class=\"fu  aaA aaB\"> </div>"),
+
+	new Shortcut("Ctrl+K", "Insert a link", "To what URL should this link go?"), // difficult; extremely long outerHTML and innerText tags
+	new Shortcut("Ctrl+Shift+C", "Add Cc recipients", "Add Cc recipients"), // doesn't work because not registered as click. Is suggested when pressing "Link to"
+	new Shortcut("Ctrl+Shift+B", "Add Bcc recipients", "Add Bcc recipients") // doesn't work because not registered as click. Is suggested when pressing "Link to"
 ];
 
 const clickHandler = function (event: MouseEvent) {
@@ -35,7 +41,7 @@ const clickHandler = function (event: MouseEvent) {
 	console.log(`You clicked on: '${outerHTML}' (outerHTML)`);
 
 	for (const shortcut of gmailShortcuts) {
-		if (innerText === shortcut.button || outerHTML === shortcut.button) {
+		if (innerText === shortcut.description || outerHTML.includes(shortcut.button)) {
 			successNotification({
 				title: `Press ${shortcut.key}`,
 				message: `For "${shortcut.description}" try pressing "${shortcut.key}" instead ;-)`,
