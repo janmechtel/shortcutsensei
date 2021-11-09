74fda5fe60034f2aa08e733307a9327a1f99418b
Jan Mechtel <jme@templafy.com>
2021-11-09 11:27:25 +0100

    split checking outerHTML and innerText

--- source/apps/gmail/gmail.ts
+++ source/apps/gmail/gmail.ts
@@ -46,50 +46,48 @@ const gmailShortcuts: Shortcut[] = [
 	// description: string; // what does this shortcut do? The description for the cheatsheet, eg 'Compose a new message'
 	// button: string; // the content ont the Text or OuterHTML, eg. 'Compose'
 	// )
-	new Shortcut("C", "Compose", "Compose"),
+	new Shortcut("C", "Compose", "Compose","Compose an email"),
 
-	new Shortcut("E", "Archive", "<div class=\"asa\"><div class=\"ar8 T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("#", "Delete", '<div class="asa"><div class="ar9 T-I-J3 J-J5-Ji"></div></div>'),
-	new Shortcut("!", "Mark as spam", "<div class=\"asa\"><div class=\"asl T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("Shift+I", "Mark as read", "Read"),
-	new Shortcut("Shift+I", "Mark as read", "bAO T-I-J3 J-J5-Ji"),
-	new Shortcut("Shift+U", "Mark as unread", "Unread"),
-	new Shortcut("Shift+U", "Mark as unread", "bAP T-I-J3 J-J5-Ji"),
+	new Shortcut("E", "Archive", "", "<div class=\"asa\"><div class=\"ar8 T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("#", "Delete",  "", "<div class=\"asa\"><div class=\"ar9 T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("!", "Mark as spam", "", "<div class=\"asa\"><div class=\"asl T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("Shift+I", "Mark as read", "Read", "bAO T-I-J3 J-J5-Ji"),
+	new Shortcut("Shift+U", "Mark as unread", "Unread", "bAP T-I-J3 J-J5-Ji"),
 	new Shortcut("=", "Mark as important", "Mark as important"),
 	new Shortcut("-", "Mark as not important", "Mark as not important"),
-	new Shortcut("L", "Label as", "<div class=\"asa\"><div class=\"asb T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("Shift+T", "Add to tasks", "<div class=\"asa\"><div class=\"Vj T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("L", "Label as", "", "<div class=\"asa\"><div class=\"asb T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("Shift+T", "Add to tasks", "", "<div class=\"asa\"><div class=\"Vj T-I-J3 J-J5-Ji\"></div></div>"),
 	new Shortcut("M", "Mute conversation", "Mute"),
 
-	new Shortcut("K", "Jump to newer email", "data-tooltip=\"Newer\""),
-	new Shortcut("J", "Jump to older email", "data-tooltip=\"Older\""),
-	new Shortcut("A", "Reply all", ">Reply all</span>"),
+	new Shortcut("K", "Jump to newer email", "", "data-tooltip=\"Newer\""),
+	new Shortcut("J", "Jump to older email", "", "data-tooltip=\"Older\""),
+	new Shortcut("A", "Reply all", "", ">Reply all</span>"),
 	new Shortcut("R", "Reply", "Reply"),
-	new Shortcut("S", "Star/unstar", "class=\"f T-KT-JX\" src=\"images/cleardot.gif"),
-	new Shortcut("F", "Forward", "role=\"link\" tabindex=\"0\" class=\"ams bkG\" jslog=\"21578; u014N:cOuCgd,Kr2w4b;\">Forward</span>"),
-	new Shortcut("U", "Go back to inbox", "<div class=\"asa\"><div class=\"ar6 T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("S", "Star/unstar", "", "class=\"f T-KT-JX\" src=\"images/cleardot.gif"),
+	new Shortcut("F", "Forward", "", "role=\"link\" tabindex=\"0\" class=\"ams bkG\" jslog=\"21578; u014N:cOuCgd,Kr2w4b;\">Forward</span>"),
+	new Shortcut("U", "Go back to inbox", "", "<div class=\"asa\"><div class=\"ar6 T-I-J3 J-J5-Ji\"></div></div>"),
 
-	new Shortcut("Ctrl+Z", "Undo", "<div class=\"te  aaA aaB\""),
-	new Shortcut("Ctrl+Y", "Redo", "<div class=\"sV  aaA aaB\""),
-	new Shortcut("Ctrl+K", "Insert a link", "submit_as_link"), // difficult; extremely long outerHTML and innerText tags
-	new Shortcut("Ctrl+B", "Bold", "<div class=\"eN  aaA aaB\""),
-	new Shortcut("Ctrl+I", "Italics", "<div class=\"e3  aaA aaB\""),
-	new Shortcut("Ctrl+U", "Underline", "<div class=\"fu  aaA aaB\""),
-	new Shortcut("Ctrl+Shift+7", "Numbered list", "<div class=\"e6  aaA aaB\""),
-	new Shortcut("Ctrl+Shift+8", "Bulleted list", "<div class=\"eO  aaA aaB\""),
-	new Shortcut("Ctrl+Shift+9", "Quote", '<div class="fa  aaA aaB"'),
-	new Shortcut("Ctrl+[", "Indent less", '<div class="e8  aaA aaB"'),
-	new Shortcut("Ctrl+]", "Indent more", '<div class="e2  aaA aaB"'),
-	new Shortcut("Alt+Shift+5", "Strikethrough", '<div class="td  aaA aaB"'),
-	new Shortcut("Ctrl+\\", "Remove formatting", '<div class="fb  aaA aaB"'),
-	new Shortcut("Ctrl+Shift+L", "Align left", '<div class="e4 aaA aaB"'),
-	new Shortcut("Ctrl+Shift+E", "Align center", '<div class="eP aaA aaB"'),
-	new Shortcut("Ctrl+Shift+R", "Align right", '<div class="fc aaA aaB"'),
+	new Shortcut("Ctrl+Z", "Undo", "", "<div class=\"te  aaA aaB\""),
+	new Shortcut("Ctrl+Y", "Redo", "", "<div class=\"sV  aaA aaB\""),
+	new Shortcut("Ctrl+K", "Insert a link", "", "submit_as_link"), // difficult; extremely long outerHTML and innerText tags
+	new Shortcut("Ctrl+B", "Bold",  "","<div class=\"eN  aaA aaB\""),
+	new Shortcut("Ctrl+I", "Italics", "", "<div class=\"e3  aaA aaB\""),
+	new Shortcut("Ctrl+U", "Underline", "", "<div class=\"fu  aaA aaB\""),
+	new Shortcut("Ctrl+Shift+7", "Numbered list", "", "<div class=\"e6  aaA aaB\""),
+	new Shortcut("Ctrl+Shift+8", "Bulleted list", "", "<div class=\"eO  aaA aaB\""),
+	new Shortcut("Ctrl+Shift+9", "Quote", "", '<div class="fa  aaA aaB"'),
+	new Shortcut("Ctrl+[", "Indent less", "", '<div class="e8  aaA aaB"'),
+	new Shortcut("Ctrl+]", "Indent more", "", '<div class="e2  aaA aaB"'),
+	new Shortcut("Alt+Shift+5", "Strikethrough", "", '<div class="td  aaA aaB"'),
+	new Shortcut("Ctrl+\\", "Remove formatting", "", '<div class="fb  aaA aaB"'),
+	new Shortcut("Ctrl+Shift+L", "Align left", "", '<div class="e4 aaA aaB"'),
+	new Shortcut("Ctrl+Shift+E", "Align center", "", '<div class="eP aaA aaB"'),
+	new Shortcut("Ctrl+Shift+R", "Align right", "", '<div class="fc aaA aaB"'),
 
 	new Shortcut("Ctrl+Enter", "Send", "Send"),
-	new Shortcut("Ctrl+Shift+D", "Discard draft", "<div class=\"og T-I-J3\" style=\"user-select: none;\"></div>"),
+	new Shortcut("Ctrl+Shift+D", "Discard draft", "", "<div class=\"og T-I-J3\" style=\"user-select: none;\"></div>"),
 
-	new Shortcut("G+K", "Go to tasks", '<div class="aT5-aOt-I-JX-Jw"'),
+	new Shortcut("G+K", "Go to tasks", "", '<div class="aT5-aOt-I-JX-Jw"'),
 
 	new Shortcut("Ctrl+Shift+C", "Add Cc recipients", "Cc"),
 	new Shortcut("Ctrl+Shift+B", "Add Bcc recipients", "Bcc"),
@@ -123,13 +121,24 @@ const clickHandler = function (event: MouseEvent) {
 			// exceptions. if there are too many of these we need to find a better solution
 			for (const shortcut of gmailShortcuts) {
 				console.debug(shortcut);
-				if (innerText === shortcut.description || outerHTML.includes(shortcut.button)) {
-					console.debug("match found");
+				if (innerText === shortcut.innerText && innerText !== "") {
+					console.debug("match found in InnerText");
 					successNotification({
 						title: `Press ${shortcut.key}`,
 						message: `For "${shortcut.description}" try pressing "${shortcut.key}" instead ;-)`,
 					});
-					break;
+					return;
+				}
+			}
+			for (const shortcut of gmailShortcuts) {
+				console.debug(shortcut);
+				if (outerHTML.includes(shortcut.outerHTML) && outerHTML !== "") {
+					console.debug("match found in outerHTML");
+					successNotification({
+						title: `Press ${shortcut.key}`,
+						message: `For "${shortcut.description}" try pressing "${shortcut.key}" instead ;-)`,
+					});
+					return;
 				}
 			}
 		}
