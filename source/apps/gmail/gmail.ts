e55a9901c11c7c0d26f2659a5d2d13cd2323c08b
LennardVaarten <l.a.vaarten@tilburguniversity.edu>
2021-11-09 22:29:59 +0100

    fixed shortcuts, added Cc / Bcc

--- source/apps/gmail/gmail.ts
+++ source/apps/gmail/gmail.ts
@@ -1,6 +1,6 @@
 require('styled-notifications');
 
-// skips contains part of the outerHTML properties of elements that should NOT display a popup when pressed (i.e. elements that are not buttons)
+// contains part of the outerHTML properties of elements that should NOT display a popup when pressed (i.e. elements that are not buttons)
 const elementsToSkip = [
 	"role=\"toolbar\"", // stop popup from appearing when pressing formatting toolbar
 	"Input tools on/off (Ctrl-Shift-K)", // stop popup from appearing when pressing menu above email
@@ -11,8 +11,10 @@ const elementsToSkip = [
 	"role=\"tablist\"", // stop popup from appearing when pressing taskbar on right side
 	"<div class=\"amn\">", // stop popup from appearing when pressing next to 'Forward' / 'Reply' buttons
 	"data-tooltip=\"More options\" aria-label=\"More options\">", // stop popup from appearing when pressing bottom bar of draft email
-	"data-tooltip=\"Select\"", // stop'Archive' popup from appearing when opening 'mark as ...' menu
+	"data-tooltip=\"Select\"", // stop 'Archive' popup from appearing when opening 'mark as ...' menu
 	"target=\"keyboard_shortcuts_help_window\"", // stop popups from appearing when pressing inside keyboard shortcut help menu
+	"data-hovercard-id", // stop 'Star/unstar' popup from appearing when pressing above 'Reply' box
+	"aria-label=\"Some recipients use services that don't support encryption (click for details)", // stop Cc popup from appearing when pressing around Bcc box
 ]
 
 import { Shortcut } from '../../shortcut';
@@ -46,31 +48,35 @@ const gmailShortcuts: Shortcut[] = [
 	// description: string; // what does this shortcut do? The description for the cheatsheet, eg 'Compose a new message'
 	// button: string; // the content ont the Text or OuterHTML, eg. 'Compose'
 	// )
-	new Shortcut("C", "Compose", "Compose","Compose an email"),
+	new Shortcut("C", "Compose", "Compose", "class=\"T-I T-I-KE L3"),
+	new Shortcut("Z", "Undo last action", "", "aria-label=\"Undo link\">Undo</span>"),
+	new Shortcut("Z", "Undo last action", "", "id=\"link_undo"), // had to add this one twice, because outerHTML is completely different when you undo sending an email
 
-	new Shortcut("E", "Archive", "", "<div class=\"asa\"><div class=\"ar8 T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("#", "Delete",  "", "<div class=\"asa\"><div class=\"ar9 T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("!", "Mark as spam", "", "<div class=\"asa\"><div class=\"asl T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("Shift+I", "Mark as read", "Read", "bAO T-I-J3 J-J5-Ji"),
-	new Shortcut("Shift+U", "Mark as unread", "Unread", "bAP T-I-J3 J-J5-Ji"),
+	new Shortcut("E", "Archive", "", "class=\"ar8 T-I-J3 J-J5-Ji"),
+	new Shortcut("#", "Delete", "", "class=\"ar9 T-I-J3 J-J5-Ji"),
+	new Shortcut("!", "Mark as spam", "", "class=\"asl T-I-J3 J-J5-Ji"),
+	new Shortcut("Shift+I", "Mark as read", "", "bAO T-I-J3 J-J5-Ji"),
+	new Shortcut("Shift+U", "Mark as unread", "", "bAP T-I-J3 J-J5-Ji"),
 	new Shortcut("=", "Mark as important", "Mark as important"),
 	new Shortcut("-", "Mark as not important", "Mark as not important"),
-	new Shortcut("L", "Label as", "", "<div class=\"asa\"><div class=\"asb T-I-J3 J-J5-Ji\"></div></div>"),
-	new Shortcut("Shift+T", "Add to tasks", "", "<div class=\"asa\"><div class=\"Vj T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("L", "Label as", "", "class=\"asb T-I-J3 J-J5-Ji"),
+	new Shortcut("Shift+T", "Add to tasks", "", "class=\"Vj T-I-J3 J-J5-Ji"),
 	new Shortcut("M", "Mute conversation", "Mute"),
 
 	new Shortcut("K", "Jump to newer email", "", "data-tooltip=\"Newer\""),
 	new Shortcut("J", "Jump to older email", "", "data-tooltip=\"Older\""),
 	new Shortcut("A", "Reply all", "", ">Reply all</span>"),
-	new Shortcut("R", "Reply", "Reply"),
+	new Shortcut("R", "Reply", "Reply", "data-tooltip=\"Reply\""),
 	new Shortcut("S", "Star/unstar", "", "class=\"f T-KT-JX\" src=\"images/cleardot.gif"),
-	new Shortcut("F", "Forward", "", "role=\"link\" tabindex=\"0\" class=\"ams bkG\" jslog=\"21578; u014N:cOuCgd,Kr2w4b;\">Forward</span>"),
-	new Shortcut("U", "Go back to inbox", "", "<div class=\"asa\"><div class=\"ar6 T-I-J3 J-J5-Ji\"></div></div>"),
+	new Shortcut("F", "Forward", "Forward"),
+	new Shortcut("U", "Go back to inbox", "", "class=\"ar6 T-I-J3 J-J5-Ji"),
 
-	new Shortcut("Ctrl+Z", "Undo", "", "<div class=\"te  aaA aaB\""),
-	new Shortcut("Ctrl+Y", "Redo", "", "<div class=\"sV  aaA aaB\""),
-	new Shortcut("Ctrl+K", "Insert a link", "", "submit_as_link"), // difficult; extremely long outerHTML and innerText tags
-	new Shortcut("Ctrl+B", "Bold",  "","<div class=\"eN  aaA aaB\""),
+	new Shortcut("CTRL+Shift+C", "Add Cc recipients", "Cc"),
+	new Shortcut("CTRL+Shift+B", "Add Bcc recipients", "Bcc"),
+
+	new Shortcut("Ctrl+Z", "Undo", "", "<div class=\"te  aaA aaB"),
+	new Shortcut("Ctrl+Y", "Redo", "", "<div class=\"sV  aaA aaB"),
+	new Shortcut("Ctrl+B", "Bold", "", "<div class=\"eN  aaA aaB\""),
 	new Shortcut("Ctrl+I", "Italics", "", "<div class=\"e3  aaA aaB\""),
 	new Shortcut("Ctrl+U", "Underline", "", "<div class=\"fu  aaA aaB\""),
 	new Shortcut("Ctrl+Shift+7", "Numbered list", "", "<div class=\"e6  aaA aaB\""),
@@ -85,7 +91,7 @@ const gmailShortcuts: Shortcut[] = [
 	new Shortcut("Ctrl+Shift+R", "Align right", "", '<div class="fc aaA aaB"'),
 
 	new Shortcut("Ctrl+Enter", "Send", "Send"),
-	new Shortcut("Ctrl+Shift+D", "Discard draft", "", "<div class=\"og T-I-J3\" style=\"user-select: none;\"></div>"),
+	new Shortcut("Ctrl+Shift+D", "Discard draft", "", "<div class=\"og T-I-J3"),
 
 	new Shortcut("G+K", "Go to tasks", "", '<div class="aT5-aOt-I-JX-Jw"'),
 
@@ -100,16 +106,10 @@ const clickHandler = function (event: MouseEvent) {
 	console.log(`You clicked on: '${innerText}' (innerText)`);
 	console.log(`You clicked on: '${outerHTML}' (outerHTML)`);
 
-	if (outerHTML.includes("Then, copy the web address from the box in your browser's address bar, and paste it into the box above.")) {
+	if (outerHTML.includes("hidden;\">Invalid email address") && outerHTML.length > 10000) {
 		successNotification({
 			title: `Press CTRL+K`,
 			message: `For "Link to" try pressing "CTRL+K" instead ;-)`,
-		});
-		return;
-	} else if (outerHTML.includes("aria-label=\"Undo link\">Undo</span>")) {
-		successNotification({
-			title: `Press Z`,
-			message: `For "Undo last action" try pressing "Z" instead ;-)`,
 		});
 		return;
 	} else {
@@ -118,9 +118,8 @@ const clickHandler = function (event: MouseEvent) {
 			console.debug(elementsToSkip.filter(skip => outerHTML.includes(skip)));
 			return;
 		} else {
-			// exceptions. if there are too many of these we need to find a better solution
 			for (const shortcut of gmailShortcuts) {
-				console.debug(shortcut);
+				// console.debug(shortcut);
 				if (innerText === shortcut.innerText && innerText !== "") {
 					console.debug("match found in InnerText");
 					successNotification({
@@ -131,7 +130,7 @@ const clickHandler = function (event: MouseEvent) {
 				}
 			}
 			for (const shortcut of gmailShortcuts) {
-				console.debug(shortcut);
+				//console.debug(shortcut);
 				if (outerHTML.includes(shortcut.outerHTML) && outerHTML !== "") {
 					console.debug("match found in outerHTML");
 					successNotification({
