11de796fa8997bb6a82dbf241c0a369c66e9475f
Jan Mechtel <jme@templafy.com>
2021-11-13 09:47:47 +0100

    general onboarding after installing the extension

--- source/background.ts
+++ source/background.ts
@@ -1,1 +1,17 @@
 import './options/options-storage.ts';
+
+browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+	// if (temporary) return; // skip during development
+	if (temporary) console.debug("temporary"); // skip during development
+	switch (reason) {
+		case "install":
+			{
+				const url = browser.runtime.getURL("onboarding/onboarding.html");
+				await browser.tabs.create({ url });
+
+				// or: await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+			}
+			break;
+		// see below
+	}
+});
