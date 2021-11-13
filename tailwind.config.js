88b17492394775ce895f26c508b92aa8990c1da9
Jan Mechtel <jme@templafy.com>
2021-11-14 00:57:48 +0100

    added tailwindcss
    https://dannyherran.com/2021/08/tailwind-css-with-parcel-v2/
    
    npm install -D tailwindcss@latest postcss@latest autoprefixer@latest postcss-import

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+			"./source/**/*.html",
+			"./source/**/*.ts",
+		],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/* ./tailwind.config.js */
