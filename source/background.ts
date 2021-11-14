a0f051399420ef4254ce34dedb6153bce36b07f8
Jan Mechtel <jme@templafy.com>
2021-11-14 11:45:04 +0100

    show onboarding also in chrome

--- source/background.ts
+++ source/background.ts
@@ -1,15 +1,15 @@
 import './options/options-storage.ts';
 
-browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+chrome.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
 	// if (temporary) return; // skip during development
 	if (temporary) console.debug("temporary"); // skip during development
 	switch (reason) {
 		case "install":
 			{
-				const url = browser.runtime.getURL("onboarding/onboarding.html");
-				await browser.tabs.create({ url });
+				const url = chrome.runtime.getURL("onboarding/onboarding.html");
+				await chrome.tabs.create({ url });
 
-				// or: await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+				// or: await chrome.windows.create({ url, type: "popup", height: 600, width: 600, });
 			}
 			break;
 		// see below
