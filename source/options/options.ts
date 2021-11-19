d760679397cddb5e641c36f7fa39f4733bbd9174
Jan Mechtel <jme@templafy.com>
2021-11-19 23:49:00 +0100

    snoozing can be started for 24h from the options

--- source/options/options.ts
+++ source/options/options.ts
@@ -3,3 +3,12 @@
 import optionsStorage from './options-storage';
 
 optionsStorage.syncForm('#options-form');
+
+// when the snooze button is pressed, save the snooze time and close the options page
+document.getElementById('snooze').addEventListener('click', () => {
+	const snoozeUntil = new Date().getTime() + 24 * 60 * 1000;
+	const snoozeUntilInput = document.querySelectorAll('input[type="number"][name^="snoozeUntil"]').item(0);
+	optionsStorage.set({snoozeUntil: snoozeUntil});
+	snoozeUntilInput.value = snoozeUntil;
+	// alert('Snooze until: ' + new Date(snoozeUntil).toLocaleString() + snoozeUntilInput.value);
+});
