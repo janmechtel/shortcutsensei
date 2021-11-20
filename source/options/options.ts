225d256852f54f40981676a89aaf6cf556b3af94
Jan Mechtel <jme@templafy.com>
2021-11-20 21:26:58 +0100

    correct snoozing state when loading options

--- source/options/options.ts
+++ source/options/options.ts
@@ -1,21 +1,39 @@
 // Don't forget to import this wherever you use it // import browser from 'webextension-polyfill';
-
 import optionsStorage from './options-storage';
 
 optionsStorage.syncForm('#options-form');
 
+async function loadSnoozeUntil() {
+	const options = await optionsStorage.getAll();
+	console.log(options);
+	setSnoozeUntil(options.snoozeUntil as number);
+}
+
 document.getElementById('snooze').addEventListener('click', () => {
-	setSnoozeUntil(24);
+	const hours = 24;
+	setSnoozeUntil(new Date().getTime() + hours * 60 * 1000);
 });
 
 document.getElementById('snoozeReset').addEventListener('click', () => {
-	setSnoozeUntil(0);
+	setSnoozeUntil(new Date().getTime());
 });
 
-function setSnoozeUntil(hours: number) {
-	//assign constant to 0 if hours is 0 otherwise assign hours to Date + hours
-	const snoozeUntil = hours === 0 ? 0 : new Date().getTime() + hours * 60 * 1000;
-	const snoozeUntilInput = document.querySelectorAll('input[type="number"][name^="snoozeUntil"]').item(0);
+function setSnoozeUntil(snoozeUntil: number) {
+
+	const snoozeOnSection = document.getElementById('snoozeOnSection');
+	const snoozeOffSection = document.getElementById('snoozeOffSection');
+	const snoozeMessageSpan = document.getElementById('snoozeMessage');
+
+	if (snoozeUntil > new Date().getTime()) {
+		snoozeOnSection.style.display = 'none';
+		snoozeOffSection.style.display = 'block';
+		snoozeMessageSpan.textContent = `Snoozing until ${new Date(snoozeUntil).toLocaleTimeString()}`;
+	} else {
+		snoozeOnSection.style.display = 'block';
+		snoozeOffSection.style.display = 'none';
+		snoozeMessageSpan.textContent = `Snooze disabled ${snoozeUntil}`;
+	}
 	optionsStorage.set({ snoozeUntil: snoozeUntil });
-	snoozeUntilInput.value = snoozeUntil;
 }
+
+loadSnoozeUntil();
